5638765cf251f696747c5a2773f108877cf32ee8
Павел Некрасов <88438487+NekrasovPS@users.noreply.github.com>
2022-10-04 23:34:03 +0400

    Add files via upload

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,4 @@
 jQuery(function ($) {
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > 24) {
-      $(".header").addClass("fixed");
-      $(".inner__main-title").css("margin-top", "198px");
-    } else if ($(this).scrollTop() < 24) {
-      $(".header").removeClass("fixed");
-      $(".inner__main-title").css("margin-top", "150px");
-    }
-  });
   $(".slider").slick({
     slidesToShow: 3,
     slidesToScroll: 1,
@@ -19,16 +10,6 @@ jQuery(function ($) {
 });
 
 $(document).ready(function () {
-  $("#menu").click(function () {
-    $("#mobile__menu").addClass("open");
-    $("body").addClass("open");
-  });
-
-  $("#close").click(function () {
-    $("#mobile__menu").removeClass("open");
-    $("body").removeClass("open");
-  });
-
   $("#tel").keyup(function (e) {
     this.value = this.value.replace(/[^0-9\.]/g, "");
   });
